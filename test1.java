e8f1bcd421bf6e758329ca05f512acdec2977dcb
Abhishek <researcherabhishek1@gmail.com>
2025-04-03 15:40:22 +0530

    remote code fixed

--- test1.java
+++ test1.java
@@ -6,11 +6,12 @@ public class numbers{
 			system.out.println("Enter two numbers");
 		int num1=sc.nextInt();
 		int num2=sc.nextInt();
-
-		int add=num1-num2;
+                int add = num1 + num2;
+                System.out.println("addition of two numbers are:")
+		int sub=num1-num2;
 		system.out.println("substraction of two numbers are:");
 
-		int add=num1*num2;
+		int mul=num1*num2;
 		system.out.println("multiplication of two numbers are:");
 
 		sc.close
